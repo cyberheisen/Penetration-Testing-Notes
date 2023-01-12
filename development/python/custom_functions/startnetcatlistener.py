aef7ae328759a4949a5d044fdd2b43565ec5abc2
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2023-01-11 21:55:56 -0600

    Update startnetcatlistener.py

--- development/python/custom_functions/startnetcatlistener.py
+++ development/python/custom_functions/startnetcatlistener.py
@@ -1,4 +1,10 @@
+import subprocess
+
 def startNetcatListener(port,path='.'):
     print("[+] Starting netcat listener")
     proc = subprocess.Popen('%s/nc -lvp %s' % (path,port), shell=True)
     return proc
+
+port = '4444'
+
+startNetcatListener(port,'/bin')
