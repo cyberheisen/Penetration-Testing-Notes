d964dff37ffd964163aa415a6a86722896bce993
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2023-01-12 10:50:57 -0600

    Create http_requests.py

---
+++ development/python/custom_functions/http_requests.py
@@ -0,0 +1,27 @@
+import requests
+from requests.exceptions import Timeout
+
+proxies = {'http':'http://127.0.0.1:8080','https':'https://127.0.0.1:8080'}
+timeout = 5 # This value can be modified for time based blind SQLi
+headers = {'Content-Type':'application/x-www-form-urlencoded'}
+
+def http_request(url,method=None,data=None,headers=None,proxies=None,timeout=None):
+     
+    try:
+        # POST Method        
+        if method.lower() == 'post':
+            res = requests.post(url,data,headers=headers,proxies=proxies,timeout=timeout)
+        
+        # Default GET Method
+        else:
+            res = requests.get(url,headers=headers,proxies=proxies,timeout=timeout)
+        return res
+        
+    except Exception as error:
+        return error
+      
+      
+# sample use
+url = 'http://www.google.com'
+response = http_request(url)
+print(response.text)
