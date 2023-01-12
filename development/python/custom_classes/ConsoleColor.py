e3dec132e9fe7e5707406149f620aa37c1c975bf
cyberheisen <62576899+cyberheisen@users.noreply.github.com>
2023-01-12 10:47:16 -0600

    Update ConsoleColor.py
    Added blue flag coloring

--- development/python/custom_classes/ConsoleColor.py
+++ development/python/custom_classes/ConsoleColor.py
@@ -3,15 +3,18 @@ class ConsoleColor:
     def __init__(self) -> None:
         pass
 
-    def print(self,text,type,tabs = 0):
+    def print(self,text,type="step",tabs = 0):  #overwrites the standard print function.
     
         # declared color variables
         tab = "    " # 4 spaces
-        step = "\033[1m[+]\033[22m "
-        backstep = "\033[1m[-]\033[22m "
-        success = "\033[32;1m[\u0394]\033[22m "
-        error = "\033[31;1m[!]\033[22m "
-        reset = "\033[0m"
+        step = "\033[1m[+]\033[22m "  # Default - Use for showing action, like starting a service
+        backstep = "\033[1m[-]\033[22m " # Used for showing negative action, live stopping a service
+        success = "\033[32;1m[\u0394]\033[22m " # Used to show something was successful
+        error = "\033[31;1m[!]\033[22m " # Used for error messages
+        reset = "\033[0m" # This is required to reset the terminal colors after a custom message is printed
+        flag = "\33[34;1m[\\O/]"  # Use for flags or major objectives
+
+        text = str(text)
 
         if type == "success":
             text = success+text
@@ -21,9 +24,12 @@ class ConsoleColor:
             text = step+text
         if type == "back_step":
             text = backstep+text
+        if type == "flag":
+            text = flag+text
         else:
             text = text
         if not tabs == 0:
             text = (tab*tabs) + text
-        
+
         print(text+reset)
+    
